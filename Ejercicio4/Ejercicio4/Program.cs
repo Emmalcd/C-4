b5c2e9564fd8bc5772fe4dfb822f1cb516ca2635
Emmalcd <joselemmanuel9@gmail.com>
2024-01-20 14:33:33 -0600

    Ejercicio 4

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            ConsoleKeyInfo tecla;
+            int ascii;
+            Console.WriteLine("Ingresar un string y presionar enter");
+            //Console.WriteLine();
+            entrada = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Usted ha ingresado {entrada}");
+            Console.WriteLine($"Ingrese una tecla y presione Enter: ");
+            tecla=Console.ReadKey();
+            Console.ReadKey();
+            Console.WriteLine();
+
+            ascii= (int)tecla.KeyChar;
+            Console.WriteLine();
+            Console.WriteLine($"La tecla {tecla.KeyChar} tiene como código ascii: {ascii}");
+            Console.Read();
+
+        }
+    }
+}
